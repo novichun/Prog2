cb5ee820a86d39e1babd038cc76520f8d4a487d6
novichun <novakdanci@gmail.com>
2021-10-03 20:27:03 +0200

    Boostrap/Routes

---
+++ ERP Vallalatiranyitas/src/resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('templates.main')
+
+@section('content')
+
+    <h1>Hi from index</h1>
+
+
+@endsection
---
+++ ERP Vallalatiranyitas/src/resources/views/templates/main.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta content='maximum-scale=1.0, initial-scale=1.0, width=device-width' name='viewport'>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{config('app.name', 'ERP Vállalatirányítási Rendszer')}}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <!-- js -->
+       <script src="{{ asset('js/app.js') }}" defer></script>
+
+    </head>
+    <body>
+
+    <nav class="navbar navbar-expand-lg ">
+                <div class="container">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">{{config('app.name', 'ERP Vállalatirányítási Rendszer')}}</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Users</a>
+                </li>
+            </ul>
+            <div class="form-inline my-2 my-lg-0">
+            @if (Route::has('login'))
+                        <div>
+                            @auth
+                                <a href="{{ url('/home') }}" >Home</a>
+                            @else
+                                <a href="{{ route('login') }}" >Log in</a>
+
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}">Register</a>
+                                @endif
+                            @endauth
+                        </div>
+                    @endif
+                </div>
+                 </div>
+            </div>
+        </div>
+</nav>
+
+       
+
+    <main class="container">
+        @yield('content')
+    </main>
+
+    </body>
+</html>
