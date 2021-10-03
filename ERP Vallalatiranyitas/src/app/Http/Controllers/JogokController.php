c14eb32aece72d20bab731378c302c03d94c3214
novichun <novakdanci@gmail.com>
2021-10-03 22:59:57 +0200

    Migration/DB_sema

---
+++ ERP Vallalatiranyitas/src/app/Http/Controllers/JogokController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jogok;
+use Illuminate\Http\Request;
+
+class JogokController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Jogok  $jogok
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Jogok $jogok)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Jogok  $jogok
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Jogok $jogok)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Jogok  $jogok
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Jogok $jogok)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Jogok  $jogok
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Jogok $jogok)
+    {
+        //
+    }
+}
